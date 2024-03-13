99972ebea98cf1b470d04fa49f2e71457b09c993
Javier Luraschi <jluraschi@gmail.com>
2024-03-12 23:30:14 -0700

    rename package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='ioflow',
+setup(name='iobind',
       version='0.1',
       description='Define inputs and outputs',
-      url='http://github.com/hal9ai/ioflow',
+      url='http://github.com/hal9ai/iobind',
       author='Javier Luraschi',
       author_email='javier@hal9.ai',
       license='MIT',
-      packages=['ioflow'],
+      packages=['iobind'],
       zip_safe=False)
