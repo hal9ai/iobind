02ee75d08f7a87f4e46aad50b1a544993bdfa9ae
Javier Luraschi <jluraschi@gmail.com>
2024-03-12 23:35:30 -0700

    start versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='iobind',
-      version='0.1',
+      version='0.0.1',
       description='Define inputs and outputs',
       url='http://github.com/hal9ai/iobind',
       author='Javier Luraschi',
